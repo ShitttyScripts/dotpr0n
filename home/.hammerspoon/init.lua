e1db8c3e51b7c05cba315cc2b87f114f2c6530ef
Marcel Bischoff <marcel@herrbischoff.com>
2016-03-29 21:30:32 +0200

    Update config

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -13,36 +13,7 @@ function reloadConfig(files)
     end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.notify.new({title="Hammerspoon", informativeText="Configuration loaded"}):send()
-
---
--- Window tiling
---
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
+hs.alert.show("Config loaded")
 
 --
 -- WiFi watcher
