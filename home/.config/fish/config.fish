34e9b62951c0e94a460ac551b6911a75c3aa1a3c
Marcel Bischoff <marcel@herrbischoff.com>
2016-04-13 22:05:08 +0200

    Remove tackle for good, never used it

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -32,24 +32,6 @@ set fish_color_search_match normal \x2d\x2dbackground\x3d75715e
 set fish_color_selection black \x2d\x2dbackground\x3dwhite
 set fish_color_valid_path \x2d\x2dunderline
 
-# Paths to your tackle
-# set tacklebox_path ~/.tackle ~/.tacklebox
-
-# Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
-# Custom modules may be added to ~/.tacklebox/modules/
-# Example format: set tacklebox_modules virtualfish virtualhooks
-
-# set tacklebox_modules virtualfish virtualhooks z
-
-# Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
-# Custom plugins may be added to ~/.tacklebox/plugins/
-# Example format: set tacklebox_plugins python extract
-
-# set tacklebox_plugins docker extract grc pip python up
-
-# Load Tacklebox configuration
-# source ~/.tacklebox/tacklebox.fish
-
 # Aliases and functions
 source $HOME/.aliases.fish
 source $HOME/.functions.fish
