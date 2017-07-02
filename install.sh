a73ad4be2f104ad9197465c9471a5e7e8fc24796
Marcel Bischoff <marcel@herrbischoff.com>
2017-07-02 16:17:59 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pkg install bash git zsh && \
-git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick && \
-git clone --recurse-submodules https://github.com/herrbischoff/dotfiles.git ~/.homesick/repos/dotfiles && \
-~/.homesick/repos/homeshick/bin/homeshick link && \
-zsh && \
-source ~/.zshrc
