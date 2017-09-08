c04a8390239eeda171865a0d6d95c7be9c6c6544
Marcel Bischoff <marcel@herrbischoff.com>
2017-09-08 05:22:29 +0200

    Fix acme functions

--- home/.config/fish/functions/acme-install.fish
+++ home/.config/fish/functions/acme-install.fish
@@ -1,1 +1,10 @@
-/root/.homesick/repos/dotfiles/home/.config/fish/functions/acme-issue.fish
+function acme-install
+    if not test -d /usr/local/etc/ssl/$argv[1]
+        command mkdir -pv /usr/local/etc/ssl/$argv[1]
+    end
+
+    ~/acme.sh/acme.sh --install-cert -d $argv[1] \
+        --key-file       /usr/local/etc/ssl/$argv[1]/key.pem  \
+        --fullchain-file /usr/local/etc/ssl/$argv[1]/cert.pem \
+        --reloadcmd     "service nginx restart"
+end
--- home/.config/fish/functions/acme-issue.fish
+++ home/.config/fish/functions/acme-issue.fish
@@ -1,10 +1,3 @@
-function acme-install
-    if not test -d /usr/local/etc/ssl/$argv[1]
-        command mkdir -pv /usr/local/etc/ssl/$argv[1]
-    end
-
-    ~/acme.sh/acme.sh --install-cert -d $argv[1] \
-        --key-file       /usr/local/etc/ssl/$argv[1]/key.pem  \
-        --fullchain-file /usr/local/etc/ssl/$argv[1]/cert.pem \
-        --reloadcmd     "service nginx restart"
+function acme-issue
+    ~/acme.sh/acme.sh --issue $argv /usr/local/www/acme.sh
 end
