b09162cf17487b3a7fc63de3f39428e5d6b6a24c
Marcel Bischoff <marcel@herrbischoff.com>
2017-09-30 04:58:47 +0200

    Update archive function

--- home/.config/fish/functions/archive.fish
+++ home/.config/fish/functions/archive.fish
@@ -1,3 +1,3 @@
 function archive
-	tar cfv - $argv[1] | pixz | ssh root@vandusen.port0.org "cat > /external/Backup/$argv[2]"
+	tar cfv - $argv[1] | pixz -9 | gpg --encrypt --recipient marcel@herrbischoff.com | ssh root@vandusen.port0.org "cat > /external/Projekte/$argv[2].txz.gpg"
 end
