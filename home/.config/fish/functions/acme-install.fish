537f181781ca84da54fecc469b0bd8c954d51392
Marcel Bischoff <marcel@herrbischoff.com>
2017-10-17 11:23:56 +0200

    Change acme.sh paths to correct ones

--- home/.config/fish/functions/acme-install.fish
+++ home/.config/fish/functions/acme-install.fish
@@ -3,7 +3,7 @@ function acme-install
         command mkdir -pv /usr/local/etc/ssl/$argv[1]
     end
 
-    ~/acme.sh/acme.sh --install-cert -d $argv[1] \
+    ~/.acme.sh/acme.sh --install-cert -d $argv[1] \
         --key-file       /usr/local/etc/ssl/$argv[1]/key.pem  \
         --fullchain-file /usr/local/etc/ssl/$argv[1]/cert.pem \
         --reloadcmd     "service nginx restart"
--- home/.config/fish/functions/acme-issue.fish
+++ home/.config/fish/functions/acme-issue.fish
@@ -1,3 +1,3 @@
 function acme-issue
-    ~/acme.sh/acme.sh --issue $argv -w /usr/local/www/acme.sh
+    ~/.acme.sh/acme.sh --issue $argv -w /usr/local/www/acme.sh
 end
