f497e2f22f9b64bb466a74dc388991b23a396dfc
Marcel Bischoff <marcel@herrbischoff.com>
2018-03-25 17:52:46 +0200

    Add man colorizing

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -51,6 +51,18 @@ else
     end
 
 
+    # Colorize man
+    set MANROFFOPT '-c'
+    set LESS_TERMCAP_mb (tput bold; tput setaf 2)
+    set LESS_TERMCAP_md (tput bold; tput setaf 6)
+    set LESS_TERMCAP_me (tput sgr0)
+    set LESS_TERMCAP_so (tput bold; tput setaf 3; tput setab 4)
+    set LESS_TERMCAP_se (tput rmso; tput sgr0)
+    set LESS_TERMCAP_us (tput smul; tput bold; tput setaf 7)
+    set LESS_TERMCAP_ue (tput rmul; tput sgr0)
+    set LESS_TERMCAP_mr (tput rev)
+    set LESS_TERMCAP_mh (tput dim)
+
     # Composer
     if test -d $HOME/.composer/vendor/bin
         set -gx PATH $PATH $HOME/.composer/vendor/bin
