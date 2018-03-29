f3cdc00958b2dd1d75460acc3a069b4ae8da594f
Marcel Bischoff <marcel@herrbischoff.com>
2018-03-29 15:27:34 +0200

    Fix archive function

--- home/.config/fish/functions/archive.fish
+++ home/.config/fish/functions/archive.fish
@@ -1,3 +1,3 @@
 function archive
-	tar cfv - $argv[1] | pixz -9 | gpg --encrypt --recipient marcel@herrbischoff.com | ssh root@vandusen.port0.org "cat > /external/Projekte/$argv[2].txz.gpg"
+	tar cfv - $argv[1] | pixz -9 | gpg --encrypt --recipient marcel@herrbischoff.com | ssh herrbischoff@vandusen.port0.org "cat > /external/Projekte/$argv[2].txz.gpg"
 end
