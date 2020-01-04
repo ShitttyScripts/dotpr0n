70f1f882649c7318ebb37f9e2701272f38178319
Marcel Bischoff <marcel@herrbischoff.com>
2020-01-04 18:54:25 +0100

    Update ASN function, add cidr-merge command

--- .config/fish/functions/asn.fish
+++ .config/fish/functions/asn.fish
@@ -1,7 +1,7 @@
 function asn
     set -l ASN $argv[1]
-    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route:' | awk '{print $2}' | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4
-    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route6:' | awk '{print $2}' | sort
+    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route:' | awk '{print $2}' | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | cidr-merge
+    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route6:' | awk '{print $2}' | sort | cidr-merge
     set -e ASN
 end
 
