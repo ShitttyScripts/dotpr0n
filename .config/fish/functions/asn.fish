79c7d7e8340cac1e98b3e4a6be66db3afda96a8f
Marcel Bischoff <marcel@herrbischoff.com>
2020-02-07 13:36:17 +0100

    Move asn function to POSIX script

--- .config/fish/functions/asn.fish
+++
@@ -1,8 +0,0 @@
-function asn
-    set -l ASN $argv[1]
-    # whois -h whois.radb.net -- "-i origin $ASN" | grep '^route:' | awk '{print $2}' | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | cidr-merge
-    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route:' | awk '{print $2}' | cidr-merge
-    whois -h whois.radb.net -- "-i origin $ASN" | grep '^route6:' | awk '{print $2}' | cidr-merge
-    set -e ASN
-end
-
